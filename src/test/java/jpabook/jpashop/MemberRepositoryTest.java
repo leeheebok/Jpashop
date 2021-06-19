4aa775d0b30871018ceb9c5f4d4cd689560727d6
heebok <lehebk@naver.com>
2021-06-19 20:47:24 +0900

    H2 Db 설정

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +16,7 @@ class MemberRepositoryTest {
     
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception{
         //given
         Member member = new Member();
@@ -27,6 +29,8 @@ class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember = member: " + (findMember == member));
     }
 
 }
