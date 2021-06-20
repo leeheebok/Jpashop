870595af8785c338f646b45cbc54accfd294f10a
heebok <lehebk@naver.com>
2021-06-20 20:32:29 +0900

    MemberServiceTest

---
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+    @Autowired EntityManager em;
+
+    @Test
+//    @Rollback(value = false)
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("Lee");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        //given
+        Member member1 = new Member();
+        member1.setName("Lee");
+
+        Member member2 = new Member();
+        member2.setName("Lee");
+
+        //when
+        memberService.join(member1);
+        try{
+            memberService.join(member2); //예외가 발생하여야한다
+        }catch (IllegalArgumentException e){
+            return;
+        }
+
+
+        //then
+        fail("예외가 발생해야 한다. ");
+
+    }
+
+}
