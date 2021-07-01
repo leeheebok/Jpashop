79d08fd03c03b5c128adac5277e852f42bd4c451
heebok <lehebk@naver.com>
2021-07-01 14:59:35 +0900

    enhance

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,14 +3,12 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
-
 
     private final EntityManager em;
 
