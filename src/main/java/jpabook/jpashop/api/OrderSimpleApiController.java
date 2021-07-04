a7859ff268e42fa6fefedd008e0da811262e7bee
heebok <lehebk@naver.com>
2021-07-04 14:56:10 +0900

    Api Development advanced V1 (Hibernate5)

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+//XtoOne(ManyToOne, OneToOne)
+//Order
+//Order -> Member
+//Order -> Delivery
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all){
+            order.getMember().getName(); //Lazy Force Reset
+            order.getDelivery().getAddress(); //Lazy Force Reset
+        }
+        return all;
+    }
+
+
+}
