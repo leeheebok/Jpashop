79fc62b90f8fb303af06cd584cbac65afc05a72c
heebok <lehebk@naver.com>
2021-07-06 10:45:06 +0900

    Order inquiry Api V6

--- src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.repository.order.query;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 public class OrderItemQueryDto {
