985d2fdd9a08c3ef38e64a45291ec552618760e8
heebok <lehebk@naver.com>
2021-08-05 23:28:18 +0900

    enhance

--- src/main/java/jpabook/jpashop/item/Movie.java
+++ src/main/java/jpabook/jpashop/item/Movie.java
@@ -5,12 +5,16 @@ import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @DiscriminatorValue("M")
 @Getter @Setter
 public class Movie extends Item {
 
+    @NotNull
     private String director;
+
+    @NotNull
     private String actor;
 }
